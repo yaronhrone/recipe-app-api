7cdb779c4ceb31e933842ced657140d8eae3f3f8
yaron <yaronhrone@gmail.com>
2026-01-29 11:58:31 +0200

    chacke code db

---
+++ app/app/calc.py
@@ -0,0 +1,11 @@
+"""
+Calculator functions
+"""
+
+def add(x,y):
+    """Add x nad y and return the result"""
+    return x + y
+
+def subtract(x,y):
+    """Subtract y from x and return the result"""
+    return x - y
