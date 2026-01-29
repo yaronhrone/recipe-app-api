de138b17bb9e2e2f96ecf3997253da57ef8820dc
yaron <yaronhrone@gmail.com>
2026-01-29 19:50:09 +0200

    Added test to wait db

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -1,11 +1,27 @@
 """
 Django command to wait for the database to be available.
 """
+import time
 
+from psycopg2 import OperationalError as Psycopg2OpError
+
+
+from django.db import connections
+from django.db.utils import OperationalError
 from django.core.management.base import BaseCommand
 
 class Command(BaseCommand):
     """Django command to wait for database."""
 
     def handle(self,*args,**options):
-        pass
+        """Entry point for command."""
+        self.stdout.write('Waiting for database...')
+        db_up = None
+        while not db_up :
+            try:
+                db_up = connections['default']
+            except (Psycopg2OpError, OperationalError):
+                self.stdout.write('Database unavailable, waiting 1 second...')
+                time.sleep(1)
+        self.stdout.write(self.style.SUCCESS('Database available!'))
+        
--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -10,25 +10,24 @@ from django.core.management import call_command
 from django.db.utils import OperationalError
 from django.test import SimpleTestCase
 
-@patch('core.management.commands.wait_for_db.Connection.check')
+@patch('django.db.utils.ConnectionHandler.__getitem__')
 class CommandTests(SimpleTestCase):
     """Test commands."""
 
-    def test_wait_for_db_ready(self, patched_check):
+    def test_wait_for_db_ready(self, patched_getitem):
         """Test wating for db when db is available."""
-        patched_check.return_value = True
+        patched_getitem.return_value = True
 
         call_command('wait_for_db')
 
-        patched_check.assert_called_once_with(database=['default'])
-
+        self.assertEqual(patched_getitem.call_count, 1)
     @patch('time.sleep')
-    def test_wait_for_db_deplay(self, patched_sleep, patched_check):
+    def test_wait_for_db_deplay(self, patched_sleep, patched_getitem):
         """Test waiting for db when getting OperationalError."""
-        patched_check.side_effect = [Psycopg2Error]*2 + \
-            [OperationalError]*3 + [True]
+        patched_getitem.side_effect = [Psycopg2Error] + \
+            [OperationalError] * 5 + [True]
         
         call_command('wait_for_db')
 
-        self.assertEqual(patched_check.call_count, 6)
-        patched_check.assert_called_with(database=['default'])
+        self.assertEqual(patched_getitem.call_count, 7)
+        
