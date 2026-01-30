4a02d6e28f68ebfa45a25c7df104bc9120e239ec
yaron <yaronhrone@gmail.com>
2026-01-30 09:25:45 +0200

    after check

--- app/app/calc.py
+++ app/app/calc.py
@@ -2,10 +2,12 @@
 Calculator functions
 """
 
-def add(x,y):
-    """Add x nad y and return the result"""
+
+def add(x, y):
+    """Add x and y and return result"""
     return x + y
 
-def subtract(x,y):
-    """Subtract y from x and return the result"""
-    return x - y
+
+def subtract(x, y):
+    """Subtract x from y and return result"""
+    return y - x
