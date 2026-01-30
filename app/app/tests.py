7257e8b8afe6687ba2f82cc95a76088320130c1a
yaron <yaronhrone@gmail.com>
2026-01-30 09:28:15 +0200

    fix clac

--- app/app/tests.py
+++ app/app/tests.py
@@ -20,4 +20,4 @@ class CalcTests(SimpleTestCase):
         """Test subtracting numbers."""
         res = calc.subtract(10, 15)
 
-        self.assertEqual(res, -5)
+        self.assertEqual(res, 5)
