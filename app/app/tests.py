e273e3a5304335f82b85670c3c9c1bd5a1235f85
yaron <yaronhrone@gmail.com>
2026-01-30 09:41:49 +0200

    fix spaces

--- app/app/tests.py
+++ app/app/tests.py
@@ -11,10 +11,10 @@ class CalcTests(SimpleTestCase):
     """Test the calc module."""
 
     def test_add_numbers(self):
-          """Test adding numbers together."""
+        """Test adding numbers together."""
 
-          res = calc.add(5, 6)
-          self.assertEqual(res, 11)
+        res = calc.add(5, 6)
+        self.assertEqual(res, 11)
 
     def test_subtract_numbers(self):
         """Test subtracting numbers."""
